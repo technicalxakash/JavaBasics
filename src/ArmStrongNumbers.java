d688a41925089cee52f056482cf819fb070a9d34
technicalxakash <akashr2468@gmail.com>
2025-05-07 23:01:25 +0530

    tables

--- src/ArmStrongNumbers.java
+++ src/ArmStrongNumbers.java
@@ -39,35 +39,47 @@ public class ArmStrongNumbers {
 		
 		
 		//with dynamic input
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the numbers : ");
-    int num=sc.nextInt();
-    int temp=num;
-	int	lastDigit=0;
-	int count=0;
-	int sum=0;
+//		Scanner sc=new Scanner(System.in);
+//		System.out.print("Enter the numbers : ");
+//    int num=sc.nextInt();
+//    int temp=num;
+//	int	lastDigit=0;
+//	int count=0;
+//	int sum=0;
+//		
+//	
+//	while(temp>0) {
+//		lastDigit=num%10;
+//		count=count+1;	
+//		temp=temp/10;
+//	}
+//	System.out.println("the number count is "+count);
+//	int power=count;
+//	temp=num;
+//			while(temp>0) {
+//				
+//			lastDigit=temp%10;
+//			
+//				lastDigit=(int) Math.pow(lastDigit,power);
+//				sum=sum+lastDigit;
+//				temp=temp/10;
+//
+//			}
+//
+// System.out.println("Armstrong numbers are : "+sum);
+//
+//	}
+//
+//}
 		
-	
-	while(temp>0) {
-		lastDigit=num%10;
-		count=count+1;	
-		temp=temp/10;
-	}
-	System.out.println("the number count is "+count);
-	int power=count;
-	temp=num;
-			while(temp>0) {
-				
-			lastDigit=temp%10;
-			
-				lastDigit=(int) Math.pow(lastDigit,power);
-				sum=sum+lastDigit;
-				temp=temp/10;
-
-			}
-
- System.out.println("Armstrong numbers are : "+sum);
-
-	}
-
-}
+		int lastdigit=0;
+		int sum=0;
+	  int num=1234;
+	  while(num>0) {
+	  lastdigit=num%10;
+	  lastdigit=(int) Math.pow(lastdigit, 4);
+	  sum=sum+lastdigit;
+	  num=num/10;
+	  }
+	  System.out.println(sum);
+	}}
