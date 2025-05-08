02041d8ba296bff6c094430a9edf55fb9e41ca75
technicalxakash <akashr2468@gmail.com>
2025-05-08 09:57:00 +0530

    new

---
+++ src/patternss.java
@@ -0,0 +1,96 @@
+
+public class patternss {
+
+	public static void main(String[] args) {
+		/*
+		for(int i=1;i<=5;i++) {
+			for(int k=1;k<=i-1;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=5-i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+output= *****
+ 		 ****
+          ***
+           **
+            *
+		
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i+1-1;j++) {
+				
+				System.out.print(j+" ");
+				
+			}
+			System.out.println();
+			}
+			
+		}
+		}
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i+1-1;j++) {
+				
+				System.out.print((char)(j+64)+" ");
+				
+			}
+			System.out.println();
+			}
+			
+		}
+		}
+A 
+A B 
+A B C 
+A B C D 
+A B C D E 
+		
+		
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=5;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+*****
+*****
+*****
+*****
+*****
+	
+		for(int i=0;i<5;i++) {
+			for(int j=1;j<=i+1;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		*
+		**
+		***
+		****
+		*****
+		*/
+		
+		for(int i=0;i<5;i++) {
+			for(int j=1;j<=5-i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		}
+		
+		
+	}
+
