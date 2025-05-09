4ae1da3791a2464f8e0c0498ad5133c462b7f4f8
technicalxakash <akashr2468@gmail.com>
2025-05-09 21:57:40 +0530

    functions

---
+++ src/FunctionsAndMethods.java
@@ -0,0 +1,26 @@
+
+public class FunctionsAndMethods {
+//      int sum=0;
+//	void add(int a ,int b) {
+//		
+//		sum=a+b;
+//		System.out.println(sum);
+//	}
+	
+	
+	static int add(int a,int b) {
+		
+		return a+b;
+		//System.out.println(sum);
+	}
+	public static void main(String[] args) {
+//		FunctionsAndMethods obj=new FunctionsAndMethods();
+//		
+//		obj.add(1,2);
+     
+		
+		//add(4,5);
+		System.out.println(add(4,5));
+	}
+
+}
