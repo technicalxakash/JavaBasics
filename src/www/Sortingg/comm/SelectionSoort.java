db4b6792a21b1cf454239140d19677d04dc661f5
technicalxakash <akashr2468@gmail.com>
2025-05-16 13:24:56 +0530

    SelectionSort

--- src/www/Sortingg/comm/SelectionSoort.java
+++ src/www/Sortingg/comm/SelectionSoort.java
@@ -1,10 +1,41 @@
 package www.Sortingg.comm;
 
+import java.util.Scanner;
+
 public class SelectionSoort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void selection(int[] a) {
+   for(int i=0;i<a.length-1;i++) {
+			
+			int min_i=i;
+			for(int j=i+1;j<a.length;j++) {
+				if(a[j]<a[min_i]) {
+					min_i=j;
+				}
 
+			}
+			
+			int temp=a[i];
+			a[i]=a[min_i];
+			a[min_i]=temp;
+	}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the size of an array :");
+		int n=sc.nextInt();
+		int [] a=new int[n];
+		
+		for(int i=0;i<a.length;i++) {
+			a[i]=sc.nextInt();
+		}
+		
+		selection(a);
+		for(Integer i:a) {
+			System.out.println(i);
+		}
+		
 	}
 
 }
